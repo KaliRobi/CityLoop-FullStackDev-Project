4812cd9da0f76437e9d3377ef66ff288b5924916
KaliRobi <66567828+KaliRobi@users.noreply.github.com>
2024-05-21 08:14:11 +0300

    Update models.py
    Models cleanup

--- CityLoop/shipmentregister/models.py
+++ CityLoop/shipmentregister/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class ShipmentDetailsModel(models.Model):
 
+    
     MOT_OPTIONS = [
         ('Bike', 'Bike'),
         ('Car', 'Car'),
@@ -22,7 +23,6 @@ class ShipmentDetailsModel(models.Model):
         default='Bike',
     )
     
-
     def __str__(self):
         return str(self.tracking_number)
 
